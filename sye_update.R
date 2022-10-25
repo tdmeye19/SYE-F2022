704577de7b794d1ed0f6047abe52bc0f4addb28a
tdmeye19 <tdmeye19@stlawu.edu>
2022-10-25 13:40:21 -0400

    Fix prediction formula, get rid of NA's in results, and iterate 1000 times for the first match.

--- sye_update.R
+++ sye_update.R
@@ -3,3 +3,12 @@
 # the upper.tri, lower.tri, and diag in case_when to determine the outcome. However
 # I am getting a little confused, and need to understand how to properly understand
 # what I want inside of the function, and what arguments the function would need to take.
+
+# Clean up the code
+# Send email by Tuesday to be looked at by wednesday
+# rerun with 10 goals
+# change matrix size
+# use row and column names to track certain results throughout the function
+
+# October 25
+# Fixed prediction formula, got rid of NA's, and iterated 1000 times for the first match.
